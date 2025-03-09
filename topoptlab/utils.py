2fa8f2ed6f451c601673032e1129fd0b2041084e
stefanhiemer <StefanHiemer@web.de>
2025-03-09 02:35:43 +0100

    Added soe preliminary utility function for coefficients of the backward differentation formula.

--- topoptlab/utils.py
+++ topoptlab/utils.py
@@ -1,4 +1,33 @@
 import numpy as np
+from scipy.linalg import solve
+from scipy.special import factorial
+
+def bdf_coefficients(k):
+    """
+    Get coefficients for backward differentiation formula BDF. Shamelessly 
+    copied from Elmer Solver manual.´and also check the formula there to understand
+    what the coefficients mean.
+
+    Parameters
+    ----------
+    k : int
+        order of BD.
+
+    Returns
+    -------
+    coeffcients : np.ndarray shape (k+1)
+        First one is multiplied with the forces and the stiffness matrix, the others on
+        the right hand side with the mass matrix and the history of the function.
+
+    """
+    if k > 6:
+        raise NotImplementedError("Not implemented yet for order higher than 6.")
+    return np.array([[1,1],
+                    [2/3,4/3,-1/3],
+                    [6/11,18/11,-9/11,2/11],
+                    [12/25,48/25,-36/25,16/25,-3/25],
+                    [60/137,300/137,-300/137,200/137,-75/137,12/137],
+                    [60/147,360/147,-450/147,400/147,-225/147,72/147,-10/147]]) 
 
 def parse_logfile_old(file):
     """
