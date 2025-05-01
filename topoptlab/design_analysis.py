75cdb49e984bc23572bae4e9479afc0398dd29c4
stefanhiemer <StefanHiemer@web.de>
2025-05-01 16:52:34 +0200

    Forgot to delete a function.

--- topoptlab/design_analysis.py
+++ topoptlab/design_analysis.py
@@ -32,26 +32,3 @@ def lengthscale_violations(x,nelx,nely,r,nelz=None):
                                  structure=structure,
                                  mode="nearest",cval=0.)-x
     return solidviolation, voidviolation
-
-def map_eltoimg(quant,nelx,nely,**kwargs):
-    """
-    Map quantity located on elements on the usual regular grid to an image.
-
-    Parameters
-    ----------
-    quant : np.ndarray, shape (n,nchannel)
-        some quantity defined on each element (e. g. element density).
-    nelx : int
-        number of elements in x direction.
-    nely : int
-        number of elements in y direction.
-
-    Returns
-    -------
-    img : np.ndarray, shape (nely,nelx,nchannel)
-        quantity mapped to image.
-
-    """
-    #
-    shape = (nely,nelx)+quant.shape[1:]
-    return quant.reshape(shape,order="F")
