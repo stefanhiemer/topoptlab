b4f0618f5a52bbfaca6d17025218a302c7487b21
stefanhiemer <StefanHiemer@web.de>
2025-05-09 21:16:19 +0200

    Minor changes for some examples for the optimizers. With the MMA the interface needs improvement.

--- applications/optimization/rosenbrock_bounded.py
+++ applications/optimization/rosenbrock_bounded.py
@@ -281,76 +281,11 @@ def demonstrate_barzilai_borwein(nvars=3,
     print("after {0} iterations".format(int(i+1)))
     return
 
-def demonstrate_mma(nvars=3,
-                    verbose=False,
-                    maxiter=2000):
-    """
-    Simple demonstration code for the use of the method of moving asymptotes
-    minimizing the rosenbrock function in the interval [-1.5,1.5].
-    
-    Parameters
-    ----------
-    nvars : float
-        number of variables.
-    
-    Returns
-    -------
-    None
-    """
-    #
-    np.random.seed(1)
-    #
-    x = np.random.rand(nvars)
-    xhist = [x.copy(),x.copy()]
-    #
-    optimizer_kw = mma_defaultkws(x.shape[0],ft=None,n_constr=0)
-    # 
-    dobj = np.zeros(x.shape)
-    #
-    for i in np.arange(maxiter):
-        #print(x)
-        #
-        obj = rosen(x)
-        dobj[:] = rosen_der(x)
-        #
-        constrs = np.array([(x**2).sum()])
-        dconstr = 2*x
-        #
-        xval = x.copy()[None].T
-        xmma,ymma,zmma,lam,xsi,eta,mu,zet,s,low,upp = update_mma(x=x,
-                                                            xold1=xhist[-1],
-                                                            xold2=xhist[-2],
-                                                            xPhys=x,
-                                                            obj=obj,
-                                                            dobj=dobj,
-                                                            constrs=constrs,
-                                                            dconstr=dconstr,
-                                                            iteration=i,
-                                                            **optimizer_kw)
-        x = xmma.copy().flatten()
-        # update asymptotes 
-        optimizer_kw["low"] = low
-        optimizer_kw["upp"] = upp
-        # delete oldest element of iteration history
-        xhist.pop(0)
-        xhist.append(xval)
-        #
-        change = np.abs(x - xhist[-1]).max()
-        #
-        if verbose:
-            print("it.: {0} obj.: {1:.10f}, ch.: {2:.10f}".format(
-                  i+1, obj, change))
-        if change <= 1e-9:
-            break
-    print("final x: ", x)
-    print("final gradient: ", dobj)
-    print("after {0} iterations".format(int(i+1)))
-    return x, dobj,i+1
-
 if __name__ == "__main__":
     
     #
     verbose = False
+    maxiter = 1e5
     #
     import sys
     if len(sys.argv)>1: 
@@ -359,7 +294,6 @@ if __name__ == "__main__":
         maxiter = int(sys.argv[2])
     #
     demonstrate_barzilai_borwein(verbose=verbose, maxiter=maxiter)
-    demonstrate_mma(verbose=verbose, maxiter=maxiter)
     demonstrate_gradient_descent(verbose=verbose, maxiter=maxiter)
     demonstrate_anderson(mix=1.,damp=1.,verbose=verbose, maxiter=maxiter)
     demonstrate_diis(mix=0.5,damp=0.5,verbose=verbose, maxiter=maxiter)
