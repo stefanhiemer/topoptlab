98762e9f8b7971b94bbb60e45dc48d0fc4d438c6
stefanhiemer <StefanHiemer@web.de>
2025-05-13 15:05:14 +0200

    added small graphic to indicate how element and node orderings are done.

--- images/regular_mesh.py
+++ images/regular_mesh.py
@@ -22,7 +22,7 @@ def plot_meshnumbering2d(nelx=4,nely=3,ndof=2,
     ndgrid = create_nrs(nelx+1,(nely+1)*ndof)
     # 
     if ax is None:
-        fig, ax = plt.subplots(figsize=(8, 6))
+        fig, ax = plt.subplots(figsize=(2.6, 2))
         show=True
     else:
         show=False
@@ -30,14 +30,14 @@ def plot_meshnumbering2d(nelx=4,nely=3,ndof=2,
     for i in range(nely):
         for j in range(nelx):
             ax.text(x=j, y=i, s=toRoman(int(eloffset+elgrid[i,j])),
-                    ha='center', va='center', fontsize=12)
+                    ha='center', va='center', fontsize=10)
     # add node numbers
     for i in range(nely+1):
         for j in range(nelx+1):
-            ax.text(x=j-0.3, y=i-0.4, 
+            ax.text(x=j-0.35, y=i-0.3, 
                     s=",".join([str(s+ndoffset) for s in \
                                 ndgrid[i*ndof:(i+1)*ndof,j]]), 
-                    ha='center', va='center', fontsize=12)
+                    ha='center', va='center', fontsize=10)
     # grid lines
     ax.set_xticks(np.arange(0.5, nelx+0.5, 1), minor=False)
     ax.set_yticks(np.arange(0.5, nely+0.5, 1), minor=False)
@@ -50,18 +50,17 @@ def plot_meshnumbering2d(nelx=4,nely=3,ndof=2,
     ax.set_xlim(-0.5, nelx-0.5)
     ax.set_ylim(nely-0.5, -0.5)
     if show:
+        plt.savefig("meshnumbering-2d.pdf",
+                    format="pdf")
         plt.show()
     return
 
 def plot_meshnumbering3d(nelx=4,nely=3,nelz=2,ndof=2):
     # create element and node numbering
     elgrid = create_nrs(nelx,nely,nelz)
-    ndgrid = create_nrs(nelx+1,(nely+1)*ndof,nelz)
-    # 
-    #fig = plt.figure(figsize=(12, 8))
-    #gs = GridSpec(3, 2, width_ratios=[1, 1])
+    ndgrid = create_nrs(nelx+1,(nely+1)*ndof,nelz+1)
     #
-    fig, axs = plt.subplots(ncols=2, nrows=nelz+1)
+    fig, axs = plt.subplots(ncols=2, nrows=nelz+1,figsize=(8, 6))
     gs = axs[1, 0].get_gridspec()
     # remove the underlying Axes
     for ax in axs[:, 0]:
@@ -74,20 +73,42 @@ def plot_meshnumbering3d(nelx=4,nely=3,nelz=2,ndof=2):
             for k in range(nelz):
                 ax1.text(x=j, y=i, z = k,
                          s=toRoman(int(elgrid[k,i,j])), 
-                         ha='center', va='center', fontsize=12)
+                         ha='center', va='center', fontsize=10)
     # add node numbers
-    #for i in range(nely+1):
-    #    for j in range(nelx+1):
-    #        for k in range(nelz+1):
-    #            ax1.text(x=j-0.3, y=i-0.4,z=k,
-    #                     s=",".join([str(s) for s in \
-    #                                 ndgrid[k,i*ndof:(i+1)*ndof,j]]), 
-    #                     ha='center', va='center', fontsize=12)
-    # grid lines
+    for i in range(nely+1):
+        for j in range(nelx+1):
+            for k in range(nelz+1):
+                ax1.text(x=j-0.3, y=i-0.4,z=k-0.4,
+                         s=",".join([str(s) for s in \
+                                     ndgrid[k,i*ndof:(i+1)*ndof,j]]), 
+                         ha='center', va='center', fontsize=10)
+    # plt grid
     ax1.set_xticks(np.arange(0.5, nelx+0.5, 1), minor=False)
     ax1.set_yticks(np.arange(0.5, nely+0.5, 1), minor=False)
     ax1.set_zticks(np.arange(0.5, nelz+0.5, 1), minor=False)
     ax1.grid(which='major', color='black', linestyle='-', linewidth=1)
+    # grid lines
+    # x
+    for j in np.arange(-0.5, nely+0.5, 1):
+        for k in np.arange(-0.5, nelz+0.5, 1):
+                ax1.plot(xs = np.array([-0.5, nelx-0.5]),
+                         ys = np.array([j,j]),
+                         zs = np.array([k,k]),
+                         color="k")
+    # y
+    for i in np.arange(-0.5, nelx+0.5, 1):
+        for k in np.arange(-0.5, nelz+0.5, 1):
+            ax1.plot(xs = np.array([i, i]),
+                     ys = np.array([-0.5, nely-0.5]),
+                     zs = np.array([k,k]),
+                     color="k")
+    # z
+    for i in np.arange(-0.5, nelx+0.5, 1):
+        for j in np.arange(-0.5, nely+0.5, 1):
+            ax1.plot(xs = np.array([i, i]),
+                     ys = np.array([j, j]),
+                     zs = np.array([-0.5, nelz-0.5]),
+                     color="k")
     # remove ticks on x and y axis
     ax1.tick_params(axis='both',which='both',
                     bottom=False, labelbottom=False,
@@ -96,18 +117,27 @@ def plot_meshnumbering3d(nelx=4,nely=3,nelz=2,ndof=2):
     ax1.set_xlim(-0.5, nelx-0.5)
     ax1.set_ylim(nely-0.5, -0.5)
     ax1.set_zlim(-0.5, nelz-0.5)
+    # axis labels
+    ax1.set_xlabel("x", fontsize=12)
+    ax1.set_ylabel("y", fontsize=12)
+    ax1.set_zlabel("z", fontsize=12)
     # 2d plots 
     for i in range(nelz+1):
-        axs[i,1].set_title("z = "+str(i))
+        axs[i,1].set_title("z = "+str(i), fontsize=12)
         plot_meshnumbering2d(nelx=nelx,nely=nely,ndof=ndof,
                              eloffset=i*nelx*nely,ndoffset=i*(nelx+1)*(nely+1),
                              ax=axs[i,1])
+        axs[i,1].set_xlabel("x", fontsize=12)
+        axs[i,1].set_ylabel("y", fontsize=12)
     # Adjust layout to prevent overlap
     fig.tight_layout()
+    plt.savefig("meshnumbering-3d.pdf",
+                format="pdf")
     plt.show()
     return
     
 if __name__ == "__main__":
     #
-    plot_meshnumbering3d(nelx=4,nely=3,ndof=2)
+    plot_meshnumbering2d(nelx=4,nely=3,ndof=2)
+    plot_meshnumbering3d(nelx=4,nely=3,ndof=3)
     
