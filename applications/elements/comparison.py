Fixed the homogenization. Found in the process however a factor of two missing in the analytical elements. Needs to be fixed next.

applications/elements/comparison.py
@@ -18,7 +18,7 @@ def compare_heatexp_iso_2d(xe = np.array([[[-1.,-1.],
                                            [1.,1.],
                                            [-1.,1.]]]),
                            DeltaT=np.ones(4)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     c = isotropic_2d()
     #
@@ -42,7 +42,7 @@ def compare_heatexp_aniso_2d(xe = np.array([[[-1.,-1.],
                                              [1.,1.],
                                              [-1.,1.]]]),
                              DeltaT=np.ones(4)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     c = np.random.rand(3,3)
     c = c + c.T
@@ -66,7 +66,7 @@ def compare_heatexp_aniso_2d(xe = np.array([[[-1.,-1.],
 def compare_heatexp_iso_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
                                            [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]]),
                            DeltaT=np.ones(8)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     c = isotropic_3d(E=1.,nu=0.3)
     #
@@ -88,7 +88,7 @@ def compare_heatexp_iso_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-
 def compare_heatexp_aniso_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
                                              [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]]),
                              DeltaT=np.ones(8)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     c = np.random.rand(6,6)
     c = c + c.T
@@ -113,7 +113,7 @@ def compare_bodyforce_2d(xe = np.array([[[-1.,-1.],
                                          [1.,-1.],
                                          [1.,1.],
                                          [-1.,1.]]])):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lf_bodyforce_2d(xe=xe)
     #
@@ -125,7 +125,7 @@ def compare_bodyforce_2d(xe = np.array([[[-1.,-1.],
 
 def compare_bodyforce_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
                                          [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]])):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lf_bodyforce_3d(xe=xe)
     #
@@ -139,7 +139,7 @@ def compare_mass_2d(xe = np.array([[[-1.,-1.],
                                     [1.,-1.],
                                     [1.,1.],
                                     [-1.,1.]]])):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lm_mass_2d(xe=xe)
     #
@@ -151,7 +151,7 @@ def compare_mass_2d(xe = np.array([[[-1.,-1.],
 
 def compare_mass_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
                                     [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]])):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lm_mass_3d(xe=xe)
     #
@@ -166,7 +166,7 @@ def compare_laplacian_2d(xe = np.array([[[-1.,-1.],
                                          [1.,1.],
                                          [-1.,1.]]]),
                          k = np.eye(2)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_poisson_2d(xe=xe,k=k)
     #
@@ -181,7 +181,7 @@ def compare_laplacian_aniso_2d(xe = np.array([[[-1.,-1.],
                                                [1.,1.],
                                                [-1.,1.]]]),
                                k = np.eye(2)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_poisson_2d(xe=xe,k=k)
     #
@@ -194,7 +194,7 @@ def compare_laplacian_aniso_2d(xe = np.array([[[-1.,-1.],
 def compare_laplacian_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
                                          [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]]),
                          k = np.eye(3)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_poisson_3d(xe=xe,k=k)
     #
@@ -211,7 +211,7 @@ def compare_laplacian_iso_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],
                                              [-1,-1,1],[1,-1,1],
                                              [1,1,1],[-1,1,1]]]),
                              k = np.eye(3)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_poisson_3d(xe=xe,k=k)
     #
@@ -228,7 +228,7 @@ def compare_laplacian_aniso_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],
                                                [-1,-1,1],[1,-1,1],
                                                [1,1,1],[-1,1,1]]]),
                          k = np.eye(3)):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_poisson_3d(xe=xe,k=k)
     #
@@ -245,7 +245,7 @@ def compare_elast_2d(xe = np.array([[[-1.,-1.],
                                      [1.,1.],
                                      [-1.,1.]]]),
                      c = isotropic_2d()):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_linear_elast_2d(xe=xe,c=c)
     #
@@ -258,7 +258,7 @@ def compare_elast_2d(xe = np.array([[[-1.,-1.],
 def compare_elast_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
                                      [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]]),
                      c = isotropic_3d()):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_linear_elast_3d(xe=xe,c=c)
     #
@@ -273,7 +273,7 @@ def compare_elast_iso_2d(xe = np.array([[[-1.,-1.],
                                      [1.,1.],
                                      [-1.,1.]]]),
                      c = isotropic_2d()):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_linear_elast_2d(xe=xe,c=c)
     #
@@ -286,7 +286,7 @@ def compare_elast_iso_2d(xe = np.array([[[-1.,-1.],
 def compare_elast_iso_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
                                      [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]]),
                      c = isotropic_3d()):
-    l = (xe.max(axis=1)-xe.min(axis=1))[0]/2
+    l = (xe.max(axis=1)-xe.min(axis=1))[0]
     #
     Ke_quad = _lk_linear_elast_3d(xe=xe,c=c)
     #

applications/elements/consistency.py
@@ -1,80 +1,14 @@
 import numpy as np
 # different elements/physics
 from topoptlab.stiffness_tensors import isotropic_2d, isotropic_3d
-from topoptlab.elements.linear_elasticity_2d import lk_linear_elast_2d,_lk_linear_elast_2d
-from topoptlab.elements.linear_elasticity_3d import _lk_linear_elast_3d,lk_linear_elast_aniso_3d
-from topoptlab.elements.poisson_2d import lk_poisson_2d,_lk_poisson_2d,lk_poisson_aniso_2d
-from topoptlab.elements.poisson_3d import lk_poisson_3d,_lk_poisson_3d,lk_poisson_aniso_3d
-from topoptlab.elements.mass_2d import _lm_mass_2d, lm_mass_symfem
-from topoptlab.elements.mass_3d import _lm_mass_3d, lm_mass_3d
-from topoptlab.elements.heatexpansion_2d import fk_heatexp_2d,_fk_heatexp_2d,fk_heatexp_aniso_2d
-from topoptlab.elements.heatexpansion_3d import fk_heatexp_3d,_fk_heatexp_3d,fk_heatexp_aniso_3d
-
-def compare_heatexp_aniso_2d(xe = np.array([[[-1.,-1.],
-                                             [1.,-1.], 
-                                             [1.,1.], 
-                                             [-1.,1.]]]),
-                             DeltaT=np.ones(4)):
-    #
-    c = np.random.rand(3,3)
-    c = c + c.T
-    #
-    a = np.random.rand(2,2)
-    a = a + a.T
-    #
-    fe_quad = _fk_heatexp_2d(xe=xe,
-                             c=c,
-                             a=a,
-                             DeltaT=DeltaT)
-    #
-    fe_analyt = fk_heatexp_aniso_2d(c=c,a=a,
-                                    DeltaT=DeltaT)
-    #
-    np.testing.assert_allclose(fe_quad[0],
-                               fe_analyt)
-    return
-
-def compare_heatexp_iso_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
-                                    [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]]),
-                           DeltaT=np.ones(8)):
-    #
-    c = isotropic_3d()
-    #
-    a = 0.05
-    #
-    fe_quad = _fk_heatexp_3d(xe=xe,
-                             c=c,
-                             a=np.eye(3)*a,
-                             DeltaT=DeltaT)
-    #
-    fe_analyt = fk_heatexp_3d(E=1.,nu=0.3,a=a,
-                              DeltaT=DeltaT)
-    #
-    np.testing.assert_allclose(fe_quad[0],
-                               fe_analyt)
-    return
-
-def compare_heatexp_aniso_3d(xe = np.array([[[-1,-1,-1],[1,-1,-1],[1,1,-1],[-1,1,-1],
-                                    [-1,-1,1],[1,-1,1],[1,1,1],[-1,1,1]]]),
-                             DeltaT=np.ones(8)):
-    #
-    c = np.random.rand(6,6)
-    c = c + c.T
-    #
-    a = np.random.rand(3,3)
-    a = a + a.T
-    #
-    fe_quad = _fk_heatexp_3d(xe=xe,
-                             c=c,
-                             a=a,
-                             DeltaT=DeltaT)
-    #
-    fe_analyt = fk_heatexp_aniso_3d(c=c,a=a,
-                                    DeltaT=DeltaT)
-    #
-    np.testing.assert_allclose(fe_quad[0],
-                               fe_analyt)
-    return
+from topoptlab.elements.linear_elasticity_2d import _lk_linear_elast_2d
+from topoptlab.elements.linear_elasticity_3d import _lk_linear_elast_3d
+from topoptlab.elements.poisson_2d import _lk_poisson_2d
+from topoptlab.elements.poisson_3d import _lk_poisson_3d
+from topoptlab.elements.mass_2d import _lm_mass_2d
+from topoptlab.elements.mass_3d import _lm_mass_3d
+from topoptlab.elements.heatexpansion_2d import _fk_heatexp_2d
+from topoptlab.elements.heatexpansion_3d import _fk_heatexp_3d
 
 def compare_mass(xe = np.array([[[-1,-1],[1,-1],[1,1],[-1,1]],
                                 [[-2,-2.1],[2.1,-2],[2,2],[-2,2]]])):

applications/fem/linear_elasticity/homogenization.py
@@ -21,7 +21,7 @@ from topoptlab.output_designs import export_vtk
 # MAIN DRIVER
 def fem_homogenization(nelx, nely, nelz=None,
                        xPhys=None, penal=3,
-                       Emax=1.0, Emin=1e-3, nu=0.3,
+                       Emax=1.0, Emin=1e-3, nu=1/3,
                        lin_solver="scipy-direct", preconditioner=None,
                        assembly_mode="full", l=1.,
                        file="homogenization",
@@ -93,11 +93,12 @@ def fem_homogenization(nelx, nely, nelz=None,
         xPhys = np.ones(n, dtype=float,order='F')
     # get element stiffness matrix, nodal forces and  element of freedom matrix
     if ndim == 2:
-        Ke = lk_linear_elast_2d(E=1.0, nu=nu,l=el_sidelengths)
+        Ke = lk_linear_elast_2d(E=1.0, nu=nu,
+                                l=el_sidelengths)
         fe = []
         eps = np.eye(3)
         for i in range(int((ndim**2 + ndim) /2)):
-            fe.append(lf_strain_2d(eps[i],E=1.0, nu=nu,l=el_sidelengths))
+            fe.append(lf_strain_2d(eps[i],E=1.0, nu=nu,l=el_sidelengths/2))
         fe = np.column_stack(fe)
         # infer nodal degrees of freedom assuming that we have 4/8 nodes in 2/3
         nd_ndof = int(Ke.shape[0]/4)
@@ -131,11 +132,13 @@ def fem_homogenization(nelx, nely, nelz=None,
     print(fe)
     #
     ndof = edofMat.max()+1
-    # find the elemental affine displacement field
-    if ndim == 2:
+    # find the imposed elemental field
+    if ndim == 2 and nd_ndof != 1:
         fixed = np.array([0,1,3])
-    else:
+    elif ndim == 3 and nd_ndof != 1:
         fixed = np.array([0,1,2,4,5,7,8])
+    elif nd_ndof == 1:
+        fixed = np.array([0])
     free = np.setdiff1d(np.arange(Ke.shape[-1]), fixed)
     u0 = np.zeros(fe.shape)
     u0[free] = solve(Ke[free,:][:,free],fe[free,:],

monolithic_codes/homogenization.py
@@ -179,7 +179,7 @@ def elementMatVec(a, b, phi):
             # Integration point
             x = xx[ii]
             y = yy[jj]
-
+            print("x",x,y)
             # Differentiated shape functions
             dNx = 1 / 4 * np.array([-(1 - y), (1 - y), (1 + y), -(1 + y)])
             dNy = 1 / 4 * np.array([-(1 - x), -(1 + x), (1 + x), (1 - x)])
@@ -192,10 +192,10 @@ def elementMatVec(a, b, phi):
                     [-b, -b, b, b]
                 ]).T
             )
-
+            print("J",J)
             detJ = J[0, 0] * J[1, 1] - J[0, 1] * J[1, 0]
             invJ = 1 / detJ * np.array([[J[1, 1], -J[0, 1]], [-J[1, 0], J[0, 0]]])
-
+            print("detJ",detJ)
             # Weight factor at this point
             weight = ww[ii] * ww[jj] * detJ
 
@@ -207,7 +207,7 @@ def elementMatVec(a, b, phi):
             dN[2, 1:8:2] = dNx
             dN[3, 1:8:2] = dNy
             B = np.dot(np.dot(L, G), dN)
-
+            print("B",B)
             # Element matrices
             keLambda += weight * (np.dot(np.dot(B.T, CLambda), B))
             keMu += weight * (np.dot(np.dot(B.T, CMu), B))
@@ -223,7 +223,12 @@ def elementMatVec(a, b, phi):
 
 def check_elementMatVec():
 
-    keLambda, keMu, feLambda, feMu = elementMatVec(1/200, 1/200, 90)
+    keLambda, keMu, feLambda, feMu = elementMatVec(1/2, 1/2, 45)
+    # case E = 1. and nu = 1/3 plain stress
+    print("ke",(keLambda + keMu) * 3/8)
+    print("fe",(feLambda + feMu) * 3/8)
+    import sys 
+    sys.exit()
     print(keLambda)
     print(keMu)
     print(feLambda)
@@ -236,12 +241,12 @@ if __name__ == "__main__":
     #
     np.random.seed(0)
     #
-    #check_elementMatVec()
+    check_elementMatVec()
     # Example usage:
     #lambda = nu*E / ( (1+nu)*(1-2*nu) ) # 2D
     #mu = E/(2*(1+nu))
     Es = [1e-3,1e0]
-    nus = [0.3,0.3]
+    nus = [1/3,1/3]
     lambda_ = []
     mu = []
     for i in range(len(Es)):

topoptlab/elements/bodyforce_2d.py
@@ -88,7 +88,7 @@ def lf_bodyforce_2d(b=np.array([0,-1]),
         nodal forces.
 
     """
-    A = l[0]*l[1]
+    A = l[0]*l[1] / 4
     return t*A*np.array([[b[0]],
                          [b[1]],
                          [b[0]],

topoptlab/elements/mass_2d.py
@@ -82,24 +82,8 @@ def lm_mass_2d(p=1.,
         element stiffness matrix.
 
     """
-    a = l[0]*l[1]
-    return p*t*np.array([[4*a/9, 2*a/9, a/9, 2*a/9],
-                         [2*a/9, 4*a/9, 2*a/9, a/9],
-                         [a/9, 2*a/9, 4*a/9, 2*a/9],
-                         [2*a/9, a/9, 2*a/9, 4*a/9]])
-
-def lm_mass_2d_legacy():
-    """
-    Create mass matrix for 2D with bilinear quadrilateral Lagrangian
-    elements. Taken from the 88 lines code and slightly modified.
-
-    Returns
-    -------
-    Ke : np.ndarray, shape (4,4)
-        element stiffness matrix.
-
-    """
-    return np.array([[1/9, 1/18, 1/36, 1/18],
-                     [1/18, 1/9, 1/18, 1/36],
-                     [1/36, 1/18, 1/9, 1/18],
-                     [1/18, 1/36, 1/18, 1/9]])
+    v = l[0]*l[1]*t
+    return p*v*np.array([[1/9, 1/18, 1/36, 1/18],
+                         [1/18, 1/9, 1/18, 1/36],
+                         [1/36, 1/18, 1/9, 1/18],
+                         [1/18, 1/36, 1/18, 1/9]])