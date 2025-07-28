filter visualisation done, but 3D could need something to enhance contrast. convolution filter runs through without bugs, but gives wrong results. Need to check detailed problem.

applications/filter/visualize_filter.py
@@ -9,7 +9,7 @@ from topoptlab.filters import visualise_filter
 from topoptlab.utils import map_eltoimg,map_imgtoel,map_eltovoxel,map_voxeltoel
 
 
-def prepare_matrix_filter(nelx,nely,nelz,rmin):
+def initialize_matrix_filter(nelx,nely,nelz,rmin):
     if nelz is None:
         ndim = 2
     else:
@@ -21,11 +21,9 @@ def prepare_matrix_filter(nelx,nely,nelz,rmin):
                    H=H,Hs=Hs)
 
 def apply_matrix_filter(x,H,Hs):
-    #x_filtered = np.asarray( H*x )
-    #print(x_filtered)
     return np.asarray(H*x/Hs)
 
-def prepare_convol_filter(nelx,nely,nelz,rmin):
+def initialize_convol_filter(nelx,nely,nelz,rmin):
     #
     if nelz is None:
         ndim = 2
@@ -68,13 +66,15 @@ if __name__ == "__main__":
     rmin = 1.5
     nelx = 6
     nely = 6
-    nelz = None
+    nelz = 6
     #
     if nelz is None:
         ndim = 2
     else:
         ndim = 3
+    #
     ax = visualise_filter(n=(nelx,nely,nelz)[:ndim],
-                          apply_filter=prepare_matrix_filter(nelx,nely,nelz,rmin))
+                          apply_filter=initialize_matrix_filter(nelx,nely,nelz,
+                                                                rmin))
     #
     plt.show()

topoptlab/filters.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.sparse import csc_array,coo_array,coo_matrix
+from scipy.sparse import csc_array,coo_matrix
 from scipy.optimize import root_scalar
 from scipy.ndimage import convolve
 
@@ -8,6 +8,10 @@ from topoptlab.elements.screenedpoisson_2d import lk_screened_poisson_2d
 from topoptlab.elements.screenedpoisson_3d import lk_screened_poisson_3d
 from topoptlab.elements.bilinear_quadrilateral import create_edofMat as create_edofMat2d
 from topoptlab.elements.trilinear_hexahedron import create_edofMat as create_edofMat3d
+from topoptlab.geometries import diracdelta
+from topoptlab.utils import map_eltoimg,map_eltovoxel
+
+from matplotlib.pyplot import subplots,figure,figaspect,show
 
 
 def assemble_matrix_filter(nelx,nely,rmin,nelz=None,
@@ -460,28 +464,28 @@ def AMfilter(x, baseplate='S', sensitivities=None):
         return np.rot90(xi, -nRot)
     else:
         # Rotate sensitivities back to original orientation if rotated
-        #print("gradient before backrotation")
-        #print(dfx)
         return np.rot90(dfx, -nRot)
-
-import matplotlib.pyplot as plt
-from topoptlab.geometries import diracdelta
-from topoptlab.utils import map_eltoimg,map_eltovoxel
 
 def visualise_filter(n,
                      apply_filter,
+                     geo=None,
                      fig_kws=None):
     """
-
+    Apply filter to a given geometry and display the original geometry next to
+    the filtered one in order to understand the effect of a filter on a given
+    geometry of design densities.
 
     Parameters
     ----------
-    apply_filter : TYPE
-        DESCRIPTION.
-    filter_params : TYPE
-        DESCRIPTION.
-    ax : TYPE, optional
-        DESCRIPTION. The default is None.
+    n : tuple
+        contains number of elements in x,y and z direction depending on number 
+        of dimensions.
+    apply_filter : callable
+        function that applies filter.
+    geo : callable or np.ndarray of shape(np.prod(n)) or None
+        geometry of design densities on which to apply filter. 
+    fig_kws : dict or None, optional
+        keywords for figure.
 
     Returns
     -------
@@ -490,22 +494,30 @@ def visualise_filter(n,
     """
     #
     ndim = len(n)
+    #
     nelx,nely,nelz = n[:ndim] + (None,None,None)[ndim:]
     #
-    dirac = diracdelta(nelx=nelx ,nely=nely, nelz=nelz,
-                       location=None )[:,None]
+    if geo is None:
+        geo = diracdelta(nelx=nelx ,nely=nely, nelz=nelz,
+                         location=None )[:,None]
+    elif callable(geo):
+        geo = diracdelta(nelx=nelx ,nely=nely, nelz=nelz,
+                         location=None )
+    elif isinstance(geo, np.ndarray):
+        geo.shape[0] = int(np.prod(n))
     #
     if ndim == 2:
         # default plot settings 2d
         if fig_kws is None:
             fig_kws = {"figsize": (8,8)}
         #
-        fig,axs = plt.subplots(1,2,**fig_kws)
+        fig,axs = subplots(1,2,**fig_kws)
         #
-        axs[0].imshow(1-map_eltoimg(quant=dirac,
+        axs[0].imshow(1-map_eltoimg(quant=geo,
                                     nelx=nelx, nely=nely),
                       cmap="grey")
-        filtered = map_eltoimg(quant=apply_filter(dirac),
+        #
+        filtered = map_eltoimg(quant=apply_filter(geo),
                                     nelx=nelx, nely=nely)
         axs[1].imshow(1-filtered,
                       cmap="grey")
@@ -518,15 +530,47 @@ def visualise_filter(n,
                                labelleft=False)
             axs[i].axis("off")
     elif ndim == 3:
+        # default plot settings 3d
+        if fig_kws is None:
+            fig_kws = {"figsize": figaspect(2.)}
         #
-        fig = plt.figure(figsize=plt.figaspect(2.))
+        fig = figure(**fig_kws)
+        # unfiltered
+        axs = []
+        axs.append(fig.add_subplot(2, 1, 1, projection='3d'))
+        axs.append(fig.add_subplot(2, 1, 2, projection='3d'))
         #
-        ax = fig.add_subplot(2, 1, 1, projection='3d')
+        dirac_voxel = map_eltovoxel(geo,
+                                    nelx=nelx, nely=nely, nelz=nelz)
         #
-        filtered = map_eltovoxel(quant=apply_filter(dirac),
-                                 nelx=nelx, nely=nely, nelz=nelz)
+        facecolors = np.ones(dirac_voxel.shape[:-1] + (4,))
+        facecolors[:,:,:,:-1] = 1 - dirac_voxel
+        facecolors[:,:,:,-1] = dirac_voxel[:,:,:,0]
         #
-    print(dirac.sum(),filtered.sum())
+        axs[0].voxels(filled = ~np.isclose(dirac_voxel[:,:,:,0], 0),
+                      facecolors=facecolors)
+        # filtered
+        filtered_voxel = map_eltovoxel(quant=apply_filter(geo),
+                                       nelx=nelx, nely=nely, nelz=nelz)
+        #
+        facecolors = np.ones(filtered_voxel.shape[:-1] + (4,))
+        facecolors[:,:,:,:-1] = 1 - filtered_voxel
+        facecolors[:,:,:,-1] = filtered_voxel[:,:,:,0]
+        #
+        axs[1].voxels(filled = ~np.isclose(filtered_voxel[:,:,:,0], 0),
+                      facecolors=facecolors)
+        #
+        for i in range(2):
+            # limits
+            for j,nel in enumerate(n):
+                axs[i].set_xlim(0,nel)
+            #    
+            axs[i].set_xlabel( "z" )
+            axs[i].set_ylabel( "y" )
+            axs[i].set_zlabel( "x" )
+        #
+    print("mass before filter operation: ", geo.sum(),"\n",
+          "mass after filter operation: ", filtered_voxel.sum())
 
-    plt.show()
+    show()
     return

topoptlab/geometries.py
@@ -156,7 +156,7 @@ def bounding_rectangle(nelx,nely,faces=["b","t","r","l"]):
     el_flags[indices] = 2
     return el_flags
 
-def slab(nelx,nely,center,widths=None, fill_value=1):
+def slab(nelx,nely,center, widths=None, fill_value=1):
     """
     Create element flags for a slab located at the specified center with the
     specified width in each dimension.
@@ -169,8 +169,9 @@ def slab(nelx,nely,center,widths=None, fill_value=1):
         number of elements in y direction.
     center : list or tuple or np.ndarray
         coordinates of slab center.
-    widths : iterable of float and None
-        width in x and y direction.
+    widths : iterable of floats and None
+        width in x and y direction. If one entry is None or it is width is None,
+        then nelx/nely is taken as width in this direction.
     fill_value: int
         value that is prescribed to elements within sphere.
 

topoptlab/topology_optimization.py
@@ -404,7 +404,7 @@ def main(nelx, nely, volfrac, penal, rmin, ft,
            loop==0:
             # update physical properties of the elements and thus the entries
             # of the elements
-            scale = simp(xPhys=xPhys,penal=penal,eps=1e-9)#(Emin+(xPhys)**penal * (E-Emin))
+            scale = simp(xPhys=xPhys,penal=penal,eps=1e-9)
             Kes = KE[None,:,:]*scale[:,:,None]
             if assembly_mode == "full":
                 # this here is more memory efficient than Kes.flatten() as it
@@ -412,10 +412,7 @@ def main(nelx, nely, volfrac, penal, rmin, ft,
                 sK = Kes.reshape(np.prod(Kes.shape))
             elif assembly_mode == "lower":
                 sK = Kes[:,assm_indcs[:,0],assm_indcs[:,1]].reshape( n*int(KE.shape[-1]/2*(KE.shape[-1]+1)))
-                #sK = Kes[:,assm_indcs[:,0]]
-                #sK = Kes[:,:,assm_indcs[:,1]].reshape( n*int(ndof/2*(ndof+1)) )
-            # Setup and solve FE problem
-            # To Do: loop over boundary conditions if incompatible
+            ### Setup and solve FE problem
             # assemble system matrix
             K = assemble_matrix(sK=sK,iK=iK,jK=jK,
                                 ndof=ndof,solver=lin_solver,
@@ -445,7 +442,7 @@ def main(nelx, nely, volfrac, penal, rmin, ft,
             u[free, :], fact, precond = solve_lin(K=K, rhs=rhs[free],
                                                   solver=lin_solver,
                                                   preconditioner=preconditioner)
-            # Objective and objective gradient
+            # objective and sensitivities with regards to object
             obj = 0
             dobj[:] = 0.
             for i in np.arange(f.shape[1]):
@@ -513,10 +510,10 @@ def main(nelx, nely, volfrac, penal, rmin, ft,
                                  Hs) / np.maximum(0.001, x)
             #dobj[:] = H @ (dc*x) / Hs / np.maximum(0.001, x)
         elif ft == 0 and filter_mode == "convolution":
-            dobj[:] = invmapping(convolve(mapping(dobj/hs),
-                                 h,
-                                 mode="constant",
-                                 cval=0)) / np.maximum(0.001, x)
+            dobj[:] = invmapping( convolve(mapping(dobj),
+                                           weights=h, axes=(0,1,2)[:ndim],
+                                           mode="constant", 
+                                           cval=0.0)) / hs / np.maximum(0.001, x)
         elif ft == 0 and filter_mode == "helmholtz":
             dobj[:] = TF.T @ lu_solve(TF@(dobj*xPhys))/np.maximum(0.001, x)
         elif ft == 1 and filter_mode == "matrix":

topoptlab/utils.py
@@ -295,8 +295,12 @@ def elid_to_coords(el,nelx,nely,nelz=None,**kwargs):
 
     Returns
     -------
-    img : np.ndarray shape (nely,nelx)
-        quantity mapped to image.
+    x : np.ndarray shape (n)
+        x coordinates.
+    y : np.ndarray shape (n)
+        y coordinates.
+    z : np.ndarray shape (n), optional
+        z coordinates only if nelz is not None.
 
     """
     # find coordinates of each element/density