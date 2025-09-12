86dd866bc61f34204437cfea10218bab49898469
stefanhiemer <StefanHiemer@web.de>
2025-09-12 21:18:24 +0200

    Now?

--- tests/test_geometryparser.py
+++ tests/test_geometryparser.py
@@ -1,10 +1,10 @@
-from topoptlab.geometry_parser import parse_cad_and_mesh
-
 import pytest
 
 @pytest.mark.skip(reason="gmsh dependency not available in CI")
 
 def test_parse_cad_and_mesh():
+    
+    from topoptlab.geometry_parser import parse_cad_and_mesh
     
     parse_cad_and_mesh(file="./tests/test_files/box.step",
                    mesh_dim=3, 
